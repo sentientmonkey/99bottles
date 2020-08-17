55eca002f82b151a1ddb73ad006c812d7a9e01eb
Scott Windsor <swindsor@gmail.com>
2020-08-17 14:45:06 -0700

    TCR

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -5,7 +5,7 @@ class Bottles
       "99 bottles of beer on the wall, 99 bottles of beer.\n" +
       "Take one down and pass it around, 98 bottles of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, 89 bottles of beer.\n" +
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
       "Take one down and pass it around, 88 bottles of beer on the wall.\n"
     end
   end
