e6bb5da20fb2f29bffb9c67a0fdf03735dbef625
Scott Windsor <swindsor@gmail.com>
2020-08-17 16:34:45 -0700

    TCR

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -27,11 +27,7 @@ class Bottles
   end
 
   def container number
-    if number == 1
-      "bottle"
-    else
-      "bottles"
-    end
+    BottleNumber.new(number).container(number)
   end
 
   def pronoun number
