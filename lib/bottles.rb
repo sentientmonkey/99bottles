96edce4cf577dc4df0b9a02878caf1736c36aa7e
Scott Windsor <swindsor@gmail.com>
2020-08-17 14:52:58 -0700

    TCR

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -9,13 +9,14 @@ class Bottles
   end
 
   def verse number
-    if number == 2
+    case number
+    when 2
       "2 bottles of beer on the wall, 2 bottles of beer.\n" +
       "Take one down and pass it around, 1 bottle of beer on the wall.\n"
-    elsif number == 1
+    when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\n" +
       "Take it down and pass it around, no more bottles of beer on the wall.\n"
-    elsif number == 0
+    when 0
       "No more bottles of beer on the wall, no more bottles of beer.\n" +
       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
