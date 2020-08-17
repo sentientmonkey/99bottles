ccc17dd426b8b87072a9f7afa1fafacc0b32f8fa
Scott Windsor <swindsor@gmail.com>
2020-08-17 16:58:30 -0700

    TCR

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -9,7 +9,8 @@ class Bottles
 
   def verse number
     bottle_number = (number == 0 ? BottleNumber0 : BottleNumber).new number
-    next_bottle_number = BottleNumber.new bottle_number.successor
+    succ = bottle_number.successor
+    next_bottle_number = BottleNumber.new succ
 
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" +
