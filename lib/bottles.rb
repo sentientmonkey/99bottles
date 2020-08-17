477429a92c88c66f2722bfa3f49b0fe57a011174
Scott Windsor <swindsor@gmail.com>
2020-08-17 14:44:50 -0700

    TCR

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,7 +1,12 @@
 
 class Bottles
-  def verse _
-    "99 bottles of beer on the wall, 99 bottles of beer.\n" +
-    "Take one down and pass it around, 98 bottles of beer on the wall.\n"
+  def verse number
+    if number == 99
+      "99 bottles of beer on the wall, 99 bottles of beer.\n" +
+      "Take one down and pass it around, 98 bottles of beer on the wall.\n"
+    else
+      "89 bottles of beer on the wall, 89 bottles of beer.\n" +
+      "Take one down and pass it around, 88 bottles of beer on the wall.\n"
+    end
   end
 end
