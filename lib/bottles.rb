2d985bd932933928294a99dac6c54ce0f9c1a786
Scott Windsor <swindsor@gmail.com>
2020-08-17 15:06:03 -0700

    TCR

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -13,7 +13,8 @@ class Bottles
     when 0
       "No more bottles of beer on the wall, " +
       "no more bottles of beer.\n" +
-      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+      "Go to the store and buy some more, " + 
+      "99 bottles of beer on the wall.\n"
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\n" +
       "Take it down and pass it around, no more bottles of beer on the wall.\n"
