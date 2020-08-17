b7088431bad585702292b482e5825baf7936ef9f
Scott Windsor <swindsor@gmail.com>
2020-08-17 15:02:58 -0700

    TCR

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -10,9 +10,6 @@ class Bottles
 
   def verse number
     case number
-    when 2
-      "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\n" +
-      "Take one down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\n" +
       "Take it down and pass it around, no more #{container(number-1)} of beer on the wall.\n"
