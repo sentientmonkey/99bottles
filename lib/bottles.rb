c0ed844479a5a358e10d32b349b2880a1366c4dd
Scott Windsor <swindsor@gmail.com>
2020-08-17 16:57:15 -0700

    TCR

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,6 +8,7 @@ class Bottles
   end
 
   def verse number
+    number == 0 ? BottleNumber0 : BottleNumber
     bottle_number = BottleNumber.new number
     next_bottle_number = BottleNumber.new bottle_number.successor
 
