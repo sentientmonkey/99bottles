4e5693ec07c6081065bed61ba57d1a7c4eb9aaaf
Scott Windsor <swindsor@gmail.com>
2020-08-17 16:42:06 -0700

    TCR

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -9,7 +9,7 @@ class Bottles
 
   def verse number
     bottle_number = BottleNumber.new number
-    "#{bottle_number.quantity.capitalize} #{container(number)} of beer on the wall, " +
+    "#{bottle_number.quantity.capitalize} #{bottle_number.container} of beer on the wall, " +
     "#{quantity(number)} #{container(number)} of beer.\n" +
     "#{action(number)}, " + 
     "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
