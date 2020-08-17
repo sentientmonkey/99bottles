78dc24e4ede01ef8d14022ff7ab0a27d111fe511
Scott Windsor <swindsor@gmail.com>
2020-08-17 14:47:35 -0700

    TCR

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,6 +8,8 @@ class Bottles
       "1 bottle of beer on the wall, 1 bottle of beer.\n" +
       "Take it down and pass it around, no more bottles of beer on the wall.\n"
     elsif number == 0
+      "No more bottles of beer on the wall, no more bottles of beer.\n" +
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
       "Take one down and pass it around, #{number-1} bottles of beer on the wall.\n"
