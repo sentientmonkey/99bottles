777a4cc1ad13271e601e0ec0e3900f192c7c708a
Scott Windsor <swindsor@gmail.com>
2020-08-18 16:56:22 -0700

    TCR

--- test/bottles_numbers_test.rb
+++ test/bottles_numbers_test.rb
@@ -3,5 +3,6 @@ require_relative 'test_helper'
 class BottleNumberTest < Minitest::Test
 
   def test_returns_correct_class_for_given_number
+    assert_equal BottleNumber0, BottleNumber.for(0).class
   end
 end
