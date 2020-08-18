691e7f6a52efb50583091a0a06a13c31427bafb1
Scott Windsor <swindsor@gmail.com>
2020-08-18 16:57:57 -0700

    TCR

--- test/bottles_numbers_test.rb
+++ test/bottles_numbers_test.rb
@@ -3,13 +3,14 @@ require_relative 'test_helper'
 class BottleNumberTest < Minitest::Test
 
   def test_returns_correct_class_for_given_number
+    # 0,1,6 are special
     assert_equal BottleNumber0, BottleNumber.for(0).class
     assert_equal BottleNumber1, BottleNumber.for(1).class
     assert_equal BottleNumber6, BottleNumber.for(6).class
 
+    # others get the default
     assert_equal BottleNumber, BottleNumber.for(3).class
     assert_equal BottleNumber, BottleNumber.for(7).class
     assert_equal BottleNumber, BottleNumber.for(43).class
-
   end
 end
