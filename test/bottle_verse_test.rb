9ad697da6123ea21fb4ca5d71c58acbdbcfae84d
Scott Windsor <swindsor@gmail.com>
2020-08-18 17:10:20 -0700

    TCR

--- test/bottle_verse_test.rb
+++ test/bottle_verse_test.rb
@@ -40,31 +40,4 @@ Go to the store and buy some more, 99 bottles of beer on the wall.
     VERSE
     assert_equal expected, ::Bottles.new.verse(0)
   end
-
-  def test_a_couple_verses
-    expected = <<-VERSES
-99 bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall.
-
-98 bottles of beer on the wall, 98 bottles of beer.
-Take one down and pass it around, 97 bottles of beer on the wall.
-VERSES
-    assert_equal expected, ::Bottles.new.verses(99, 98)
-  end
-
-  def test_a_few_verses
-    expected = <<-VERSES
-2 bottles of beer on the wall, 2 bottles of beer.
-Take one down and pass it around, 1 bottle of beer on the wall.
-
-1 bottle of beer on the wall, 1 bottle of beer.
-Take it down and pass it around, no more bottles of beer on the wall.
-
-No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.
-VERSES
-    assert_equal expected, ::Bottles.new.verses(2, 0)
-  end
-
-
 end
