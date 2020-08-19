ec2ec46b7faf1ec94d5897934c24c3a85176ed17
Scott Windsor <swindsor@gmail.com>
2020-08-19 10:38:07 -0700

    TCR

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,9 @@
 gem 'minitest', '~> 5.4'
 require 'minitest/autorun'
 require_relative '../lib/bottles'
+
+module VerseRoleTest
+  def test_plays_verse_role
+    assert_respond_to @role_player, :lyrics
+  end
+end
