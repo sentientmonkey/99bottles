9658952c2aab85ba9ea91ebc82ec5fab68155f4d
Scott Windsor <swindsor@gmail.com>
2020-08-18 17:14:06 -0700

    TCR

--- test/bottle_verse_test.rb
+++ test/bottle_verse_test.rb
@@ -9,6 +9,14 @@ VERSE
     assert_equal expected, BottleVerse.lyrics(99)
   end
 
+  def test_general_rule_lower_bound
+    expected = <<-VERSE
+3 bottles of beer on the wall, 3 bottles of beer.
+Take one down and pass it around, 2 bottles of beer on the wall.
+    VERSE
+    assert_equal expected, BottleVerse.lyrics(3)
+  end
+
   def test_another_verse
     expected = <<-VERSE
 89 bottles of beer on the wall, 89 bottles of beer.
