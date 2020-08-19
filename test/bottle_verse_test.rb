e926d08caf2059b0cb61fd1da3f616aaf38e62d7
Scott Windsor <swindsor@gmail.com>
2020-08-18 17:52:59 -0700

    TCR

--- test/bottle_verse_test.rb
+++ test/bottle_verse_test.rb
@@ -17,14 +17,6 @@ Take one down and pass it around, 2 bottles of beer on the wall.
     assert_equal expected, BottleVerse.lyrics(3)
   end
 
-  def test_another_verse
-    expected = <<-VERSE
-89 bottles of beer on the wall, 89 bottles of beer.
-Take one down and pass it around, 88 bottles of beer on the wall.
-VERSE
-    assert_equal expected, BottleVerse.lyrics(89)
-  end
-
   def test_verse_2
     expected = <<-VERSE
 2 bottles of beer on the wall, 2 bottles of beer.
