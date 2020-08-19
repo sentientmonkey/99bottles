0a836526d6676cf8df8a2d349ece87bdaf0c7125
Scott Windsor <swindsor@gmail.com>
2020-08-18 17:10:29 -0700

    TCR

--- test/bottle_verse_test.rb
+++ test/bottle_verse_test.rb
@@ -14,7 +14,7 @@ VERSE
 89 bottles of beer on the wall, 89 bottles of beer.
 Take one down and pass it around, 88 bottles of beer on the wall.
 VERSE
-    assert_equal expected, ::Bottles.new.verse(89)
+    assert_equal expected, BottleVerse.lyrics(89)
   end
 
   def test_verse_2
@@ -22,7 +22,7 @@ VERSE
 2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall.
     VERSE
-    assert_equal expected, ::Bottles.new.verse(2)
+    assert_equal expected, BottleVerse.lyrics(2)
   end
 
   def test_verse_1
@@ -30,7 +30,7 @@ Take one down and pass it around, 1 bottle of beer on the wall.
 1 bottle of beer on the wall, 1 bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall.
     VERSE
-    assert_equal expected, ::Bottles.new.verse(1)
+    assert_equal expected, BottleVerse.lyrics(1)
   end
 
   def test_verse_0
@@ -38,6 +38,6 @@ Take it down and pass it around, no more bottles of beer on the wall.
 No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
     VERSE
-    assert_equal expected, ::Bottles.new.verse(0)
+    assert_equal expected, BottleVerse.lyrics(0)
   end
 end
