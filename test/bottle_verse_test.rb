9ce8298322aff157f7d58198c4a3034acf11e21f
Scott Windsor <swindsor@gmail.com>
2020-08-18 17:07:12 -0700

    TCR

--- test/bottle_verse_test.rb
+++ test/bottle_verse_test.rb
@@ -1,4 +1,12 @@
 require_relative "test_helper"
 
 class BottleVerseTest < Minitest::Test
+  def test_the_first_verse
+    expected = <<-VERSE
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+VERSE
+    assert_equal expected, Bottles.new.verse(99)
+  end
+
 end
