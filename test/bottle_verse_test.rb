67d4b7ec5f6c464f7198cda37ec4b3bc4c3e5f88
Scott Windsor <swindsor@gmail.com>
2020-08-18 17:05:39 -0700

    TCR

---
+++ test/bottle_verse_test.rb
@@ -0,0 +1,4 @@
+require_relative "test_helper"
+
+class BottleVerseTest < Minitest::Test
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,3 @@
+gem 'minitest', '~> 5.4'
+require 'minitest/autorun'
+require_relative '../lib/bottles'
